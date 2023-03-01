226af5ab4d8cc718a24069a1d0c9b5727dffafce
Teuton Roy <popeyehk404@gmail.com>
2023-03-01 18:47:02 +0530

    Route Heroimg with Home.js

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -1,1 +1,14 @@
 import './Heroimg.css';
+
+
+import React from 'react'
+
+const Heroimg = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Heroimg
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Heroimg from '../components/Heroimg'
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Heroimg></Heroimg>
     </div>
   )
 }
