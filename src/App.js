d95380913884c8933904232295d7cb09e406dbe8
Teuton Roy <popeyehk404@gmail.com>
2023-03-01 13:16:25 +0530

    all components add to parent class called App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Project from './components/Project';
 
 function App() {
   return (
