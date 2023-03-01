615c9f34bbb830bcd74fd915f3d2c211edd2c646
Teuton Roy <popeyehk404@gmail.com>
2023-03-01 11:22:45 +0530

    setting up with NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import {Navbar, Container} from 'react-bootstrap';
 
-function BasicExample() {
+function NavBar() {
     return (
       <Navbar bg="light" expand="lg">
         <Container>
@@ -10,20 +10,11 @@ function BasicExample() {
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     );
   }
+
+export default NavBar;
