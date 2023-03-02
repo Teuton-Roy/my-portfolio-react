28fd4f6a006b08bff227ffa7b6aebe68225b0650
Teuton Roy <popeyehk404@gmail.com>
2023-03-02 11:17:55 +0530

    rander pricingcard in project page

---
+++ src/components/PricingCard.js
@@ -0,0 +1,14 @@
+import './PricingCard.css';
+
+import React from 'react'
+
+
+const PricingCard = () => {
+  return (
+    <div>
+      <h1>Pricing card</h1>
+    </div>
+  )
+}
+
+export default PricingCard
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Proj from '../components/Proj'
+import PricingCard from '../components/PricingCard'
 
 const Project = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Proj heading="PROJECTS." text='Some of my most recent works'></Proj>
+      <PricingCard></PricingCard>
       <Footer></Footer>
     </div>
   )
