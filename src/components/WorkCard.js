924398cb5952867283c4d33ddd3974c9e2f60035
Teuton Roy <popeyehk404@gmail.com>
2023-03-02 17:39:06 +0530

    change css

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -9,7 +9,7 @@ const WorkCard = () => {
         <h1 className='project-heading'>Projects</h1>
         <div className="project-container">
             <div className="project-card">
-                <img src={pro1} alt="image" />
+                <img src={pro1} alt=""/>
                 <h2 className='project-title'>
                     Project Title
                 </h2>
