8a5d799fe98d553e89dc34d5ce0ba02ba723c3b9
Teuton Roy <popeyehk404@gmail.com>
2023-03-02 11:08:07 +0530

    set Props to Contact, Project, About pages

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-
+import Proj from '../components/Proj'
 
 const About = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Proj heading='About.' text='Im a friendly Front-End Developer.'></Proj>
       <Footer></Footer>
     </div>
   )
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-
+import Proj from '../components/Proj'
 
 const Contact = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Proj heading='Get-In-Touch' text='Feel free to contect me for any work or suggestions below'></Proj>
       <Footer></Footer>
     </div>
   )
