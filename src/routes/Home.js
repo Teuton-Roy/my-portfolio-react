ef7a32b79a48838aa77061defaaa23cf9c49ad8b
Teuton Roy <popeyehk404@gmail.com>
2023-03-02 23:46:54 +0530

    rander project work on home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Heroimg from '../components/Heroimg'
 import Footer from '../components/Footer'
+import Work from '../components/Work'
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Heroimg></Heroimg>
+      <Work></Work>
       <Footer></Footer>
     </div>
   )
