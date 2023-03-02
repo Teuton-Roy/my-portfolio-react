ee6c2e0751be4669403c73b22e0a769af04ac131
Teuton Roy <popeyehk404@gmail.com>
2023-03-02 10:29:57 +0530

    creating project page and rander with Project.js

---
+++ src/components/Proj.js
@@ -0,0 +1,21 @@
+import './Proj.css'
+
+
+import React from 'react'
+
+const Proj = () => {
+  return (
+    <div className='hero-img'>
+      <div className="heading">
+        <h1>
+          This is Heading
+        </h1>
+        <p>
+          
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Proj
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-
+import Proj from '../components/Proj'
 
 const Project = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Footer></Footer>
+      <Proj></Proj>
     </div>
   )
 }
