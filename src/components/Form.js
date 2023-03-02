e98a83d6b4cf3f1a5de1e332c0e4559c435c30ca
Teuton Roy <popeyehk404@gmail.com>
2023-03-02 23:42:19 +0530

    complete to make with contact form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,8 +4,22 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>
-        
+    <div className='form'>
+        <form>
+            <label>Your Name</label>
+            <input type='text' required='required'></input>
+
+            <label>Email</label>
+            <input type='email' required='required'></input>
+            
+            <label>Subject</label>
+            <input type='text' required='required'></input>
+            
+            <label>Massage</label>
+            <textarea rows="6" placeholder='Type Your Massage Here..'></textarea>
+
+            <button className='btn'>Submit</button>
+        </form>
     </div>
   )
 }
